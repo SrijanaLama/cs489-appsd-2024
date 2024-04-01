054cf660f9b90f7d6becb545198ee2ae184fab09
SrijanaLama <srijanalama94@gmail.com>
2024-04-01 15:56:07 -0500

    Lab1

---
+++ Lab1/ProductMgmtApp/src/main/java/edu/miu/cs/cs489apsd/lab1a/productmgmtapp/ProductMgmtApp.java
@@ -0,0 +1,68 @@
+package edu.miu.cs.cs489apsd.lab1a.productmgmtapp;
+
+import edu.miu.cs.cs489apsd.lab1a.productmgmtapp.model.Product;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+public class ProductMgmtApp {
+
+    public static void main(String[] args) {
+
+        Product product1 = new Product(3128874119L,"Banana", LocalDate.of(2023,01 ,24),125,0.55);
+        Product product2 = new Product(2927458265L,"Apple",LocalDate.of(2022,12 ,9) ,18,1.09);
+        Product product3 = new Product(9189927460L,"Carrot",LocalDate.of(2023,03,31),89,2.99);
+
+        Product[] products = new Product[]{
+                product1,product2,product3
+        };
+
+        Arrays.stream(products).sorted();
+        printProducts(products);
+    }
+
+
+
+
+    public static void  printProducts(Product[] products){
+
+        System.out.println("JSON-formatted list of all Products: ");
+
+        for(Product p : products){
+
+            System.out.println(p);
+
+        }
+
+        System.out.println("XML-formatted list of all Products: ");
+        System.out.println("<Products>");
+        for(Product p : products){
+            System.out.println("  <product>");
+            System.out.println("    <id>" + p.getProductId() + "</id>");
+            System.out.println("    <name>" + p.getName() + "</name>");
+            System.out.println("    <DateSupplied>" + p.getDateSupplied() + "</Date Supplied>");
+            System.out.println("    <QuantityInStock>" + p.getQuantityInStock() + "</QuantityInStock>");
+            System.out.println("    <UnitPrice>" + p.getUnitPrice() + "</UnitPrice>");
+            System.out.println("  </product>");
+
+        }
+        System.out.println("</products>");
+
+
+
+        System.out.println("Comma-Separated Values (CSV)-formatted list of all Products ");
+
+        for (Product product : products) {
+            System.out.println(product.getProductId() + "," + product.getName() + "," +
+                     product.getDateSupplied() + "," +
+                    product.getQuantityInStock() + "," +
+                    product.getUnitPrice()
+
+
+            );
+        }
+
+
+    }
+
+}
